b7cd398babde3dabc92e7ff556609af5b9add4ab
RubenKaptein <rubenkapteintje@gmail.com>
2025-02-06 17:46:15 +0100

    start

---
+++ javascript/javascript.js
@@ -0,0 +1,10 @@
+var button = document.querySelector("#contact")
+
+button.addEventListener("click", myfunction)
+
+function myfunction()
+{
+    alert("Mail: kapteinstudios@gmail.com\nTelefoonnummer: +31 6 27518455")
+}
+
+
