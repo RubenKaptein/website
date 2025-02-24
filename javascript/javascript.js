f41658fb95efc04fb5139e28b5f962b89595d373
RubenKaptein <rubenkapteintje@gmail.com>
2025-02-24 23:51:47 +0100

    websiteblokken en slider
    bezig geweest voor de les van morgen

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -8,3 +8,52 @@ function myfunction()
 }
 
 
+// w3schools slider
+
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+    }
+    x[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " w3-opacity-off";
+  }
+
+  let slideIndex = 1;
+showDivs(slideIndex);
+
+// Functie om naar een specifieke slide te gaan
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+// Functie om de slides te tonen/verbergen
+function showDivs(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+
+  // Verberg alle slides
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  // Maak alle indicator-afbeeldingen minder zichtbaar
+  for (i = 0; i < dots.length; i++) {
+    dots[i].style.opacity = "0.6";
+  }
+
+  // Toon de actieve slide en highlight de geselecteerde indicator
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].style.opacity = "1";
+}
