80e30a4a210680fe4630d43ff9bd0691695ea6a3
RubenKaptein <rubenkapteintje@gmail.com>
2025-03-18 15:13:45 +0100

    kaartdata
    online met niene

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -10,8 +10,6 @@ new maplibregl.Marker()
   .setPopup(new maplibregl.Popup().setText('Fitis 34'))
   .addTo(mapLibre);
 
-
-
  var map = L.map('map').setView([52.350784, 5.264702], 13); 
         
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -21,3 +19,62 @@ new maplibregl.Marker()
         L.marker([52.350784, 5.264702]).addTo(map)
             .bindPopup('Aeres Hogeschool, Almere')
             .openPopup();
+
+// GeoJSON
+var mijnGeojson =
+  {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            [
+              [
+                5.536707525185932,
+                52.740742916137265
+              ],
+              [
+                5.536707525185932,
+                52.6106899466925
+              ],
+              [
+                5.682287693725101,
+                52.6106899466925
+              ],
+              [
+                5.682287693725101,
+                52.740742916137265
+              ],
+              [
+                5.536707525185932,
+                52.740742916137265
+              ]
+            ]
+          ],
+          "type": "Polygon"
+        }
+      }
+    ]
+  }
+
+  // Kaart klaar met laden
+  map.on('load', function () {
+// voeg geojson bron toe
+    map.addSource('Urk', {
+      'type': 'geojson',
+      'data': mijnGeojson
+    });
+
+
+        // stijl op die maponload
+        map.addLayer({
+          'id': 'geojson-polygon',
+          'type': 'fill',
+          'Urk': 'punten',
+          'layout': {},
+          'paint': {
+          'fill-color': '#ff0000'
+          }
+        });
